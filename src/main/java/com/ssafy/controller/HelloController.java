f4be59699ede6dd9df6a847bc1816768eee1f53d
최재웅 <jason967@naver.com>
2020-11-18 22:25:12 +0900

    first commit

---
+++ src/main/java/com/ssafy/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ssafy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/")
+	public String baseUrl(Model model)
+	{
+		model.addAttribute("msg","테스트 페이지 입니다");
+		return "hello";
+	}
+
+}
---
+++ src/main/java/com/ssafy/dao/PageCriteria.java
@@ -0,0 +1,44 @@
+package com.ssafy.dao;
+
+/**
+ * 페이징 처리하는 컨트롤러
+ * @author choi
+ * 
+ */
+public class PageCriteria {
+
+	private int pageNum;
+	private int amount;
+
+	public PageCriteria() {
+		this(1, 10);
+	}
+
+	public PageCriteria(int pageNum, int amount) {
+		super();
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+
+	public int getPageNum() {
+		return pageNum;
+	}
+
+	public void setPageNum(int pageNum) {
+		this.pageNum = pageNum;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "PageCriteria [pageNum=" + pageNum + ", amount=" + amount + "]";
+	}
+
+}
